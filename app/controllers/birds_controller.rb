165746313166770a3fc01380ce0a32a682b4a052
Ethan Lee <wxliyc@gmail.com>
2022-04-25 11:38:24 -0500

    commit tree afc3ca5f689d2abae2afa84090dd9c67bcea2940

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,4 +22,9 @@ class BirdsController < ApplicationController
     end
   end
 
+  def create
+    bird = Bird.create(params.permit(:name, :species))
+    render json: bird, status: :created
+  end
+
 end
